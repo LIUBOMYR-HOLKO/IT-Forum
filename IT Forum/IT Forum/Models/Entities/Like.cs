6018924d83ce26cca9fa24631f941af22d8f1408
LIUBOMYR-HOLKO <golko04102001@gmail.com>
2022-03-22 22:00:01 +0200

    Created models according to ER Connect DB to project using EntityFramework

---
+++ IT Forum/IT Forum/Models/Entities/Like.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IT_Forum.Models.Entities
+{
+    public class Like
+    {
+        public String UserId { get; set; }
+        public User User { get; set; }
+
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
---
+++ IT Forum/IT Forum/Models/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using IT_Forum.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IT_Forum.Models
+{
+    public class User : IdentityUser
+    {
+        public List<Post> OneToManyPosts { get; set; }
+        public ICollection<Post> ManyToManyPosts { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<Like> Likes { get; set; }
+    }
+}
