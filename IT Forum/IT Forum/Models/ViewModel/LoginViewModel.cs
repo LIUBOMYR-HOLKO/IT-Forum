35f34a17847c07b06bb642d80a236b897a44ad27
LIUBOMYR-HOLKO <golko04102001@gmail.com>
2022-03-22 22:11:14 +0200

    Create ViewModel for Login page

---
+++ IT Forum/IT Forum/Models/ViewModel/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IT_Forum.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+        public String Email { get; set; }
+        public String Password { get; set; }
+    }
+}
